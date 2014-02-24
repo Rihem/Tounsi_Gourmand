997e839a02708088546db0709181f34a1d6c4890
hajer.BENSALEM <hajer.BENSALEM@hajerBENSALEM>
2014-02-24 20:15:18 +0100

    par hajer

---
+++ src/gourmand/dao/EspaceGourmandDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gourmand.dao;
+
+/**
+ *
+ * @author hajer.BENSALEM
+ */
+public class EspaceGourmandDAO {
+    ///dima tnasnes mathama chay gayel
+}
