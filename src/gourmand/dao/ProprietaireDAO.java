e671bf1d2baaa606dbc471bb1907283f69379d10
hajer.BENSALEM <hajer.BENSALEM@hajerBENSALEM>
2014-02-25 16:26:53 +0100

    hajer

--- src/gourmand/dao/ProprietaireDAO.java
+++ src/gourmand/dao/ProprietaireDAO.java
@@ -6,33 +6,37 @@
 
 package gourmand.dao;
 
-import gourmand.entities.Client;
 import gourmand.entities.ProprietaireEspaceGourmand;
 import gourmand.util.MyConnection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 /**
  *
  * @author hajer.BENSALEM
  */
 public class ProprietaireDAO implements Crud {
-
+ private static int idConnexion;
     @Override
     public void ajouter(Object o) {
         String url = " INSERT INTO Proprietaireespacegourmand(nom,prenom,login,password,email,tel,sexe,age) VALUES (?,?,?,?,?,?,?,?)";
         try {
             PreparedStatement prst = MyConnection.getInstance().conn.prepareStatement(url);
-            ProprietaireEspaceGourmand p= ( ProprietaireEspaceGourmand) o;
-            prst.setString(1, p.getNom());
-            prst.setString(2, p.getPrenom());
-            prst.setString(3, p.getLogin());
-            prst.setString(4, p.getPassword());
-            prst.setString(5, p.getEmail());
-            prst.setInt(6, p.getTel());
-            prst.setString(7, p.getSexe());
-            prst.setInt(8, p.getAge());
+            System.out.println("prst"+prst);
+            ProprietaireEspaceGourmand pr= ( ProprietaireEspaceGourmand) o;
+            prst.setString(1, pr.getNom());
+            prst.setString(2, pr.getPrenom());
+            prst.setString(3, pr.getLogin());
+            prst.setString(4, pr.getPassword());
+            prst.setString(5, pr.getEmail());
+            prst.setInt(6, pr.getTel());
+            prst.setString(7, pr.getSexe());
+            prst.setInt(8, pr.getAge());
             prst.executeUpdate();
             System.out.println("Insertion effectuée!");
         } catch (SQLException ex) {
@@ -42,16 +46,48 @@ public class ProprietaireDAO implements Crud {
 
     @Override
     public void supprimer(Object o) {
+        ProprietaireDAO Pdao = new ProprietaireDAO();
+        String url = " DELETE FROM  Proprietaireespacegourmand WHERE Idproprietaire=" +Pdao.idConnexion;
+        try {
+            PreparedStatement prst = MyConnection.getInstance().conn.prepareStatement(url);
+            ProprietaireEspaceGourmand Pr= (ProprietaireEspaceGourmand) o;
+
+            prst.executeUpdate();
+            System.out.println("Suppression reussit!");
+        } catch (SQLException ex) {
+            System.err.println("Pas de suppression");
+        }
     }
 
     @Override
     public void modifier(Object o) {
+        try {
+            String url = " UPDATE client SET nom=? , prenom=?,login=?,password=?,email=?,tel=?,sexe=?,age=? WHERE numCompte=" + idConnexion;
+            PreparedStatement prst = MyConnection.getInstance().conn.prepareStatement(url);
+            ProprietaireEspaceGourmand pr= (ProprietaireEspaceGourmand) o;
+            prst.setString(1, pr.getNom());
+            prst.setString(2, pr.getPrenom());
+            prst.setString(3, pr.getLogin());
+            prst.setString(4, pr.getPassword());
+            prst.setString(5, pr.getEmail());
+            prst.setInt(6, pr.getTel());
+            prst.setString(7, pr.getSexe());
+            prst.setInt(8, pr.getAge());
+            prst.executeUpdate();
+            System.out.println("MOdifier :)");
+        } catch (SQLException ex) {
+            System.err.println("Pas de modification :(");
+            
+        }
     }
+
+    
 
     @Override
     public List display() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
    
     
 }
